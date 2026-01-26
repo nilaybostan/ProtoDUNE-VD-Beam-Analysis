19b58d7129df0eefc87c99a2c3d04003d46ba260
Nilay Bostan <83247740+nilaybostan@users.noreply.github.com>
2026-01-26 12:10:01 +0300

    Update evt_plot_rawdigits.py

--- evt_plot_rawdigits.py
+++ evt_plot_rawdigits.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #to run: chmod +x plot_rawdigits.py then ./plot_rawdigits.py -f input.root ya da mesela in apptainer use for running: python dump_detsim_stage1_np02.py -f np02vd_raw_run040014_0010_df-s04-d3_dw_0_20251011T181420_reco_stage1_20251011T195216_keepup.root
-
+# 0-951,952-2*952 -1, 2*952-3072 for induction 1, induction 2, and collection
 from argparse import ArgumentParser as ap
 import ROOT as RT
 from gallery_utils import read_header, provide_list
